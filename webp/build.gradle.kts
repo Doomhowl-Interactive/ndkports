35b829c845bbedfbf1eecf0613a68efd88cf337b
bramtechs <bramtech@telenet.be>
2025-01-26 20:34:12 +0100

    try porting webp

---
+++ webp/build.gradle.kts
@@ -0,0 +1,84 @@
+import com.android.ndkports.AndroidExecutableTestTask
+import com.android.ndkports.CMakeCompatibleVersion
+import com.android.ndkports.MesonPortTask
+
+val portVersion = "1.5.0"
+
+group = "com.google.webp"
+version = "$portVersion${rootProject.extra.get("snapshotSuffix")}"
+
+plugins {
+    id("maven-publish")
+    id("com.android.ndkports.NdkPorts")
+    distribution
+}
+
+ndkPorts {
+    ndkPath.set(File(project.findProperty("ndkPath") as String))
+    source.set(project.file("src.tar.gz"))
+    minSdkVersion.set(21)
+}
+
+val buildTask = tasks.register<MesonPortTask>("buildPort")
+
+tasks.prefabPackage {
+    version.set(CMakeCompatibleVersion.parse(portVersion))
+
+    modules {
+        create("webp")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["prefab"])
+            pom {
+                name.set("webp")
+                description.set("Libwebp for .webp image support.")
+                url.set(
+                    "https://android.googlesource.com/platform/tools/ndkports"
+                )
+                licenses {
+                    license {
+                        name.set("fill in")
+                        url.set("")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Google")
+                    }
+                }
+                scm {
+                    url.set("https://android.googlesource.com/platform/tools/ndkports")
+                    connection.set("scm:git:https://android.googlesource.com/platform/tools/ndkports")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("${project.rootDir}/build/www")
+        }
+    }
+}
+
+distributions {
+    main {
+        contents {
+            from("${project.buildDir}/repository")
+            include("**/*.aar")
+            include("**/*.pom")
+        }
+    }
+}
+
+tasks {
+    distZip {
+        dependsOn("publish")
+        destinationDirectory.set(File(rootProject.buildDir, "distributions"))
+    }
+}
