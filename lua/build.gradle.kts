a08b45f9fceb5ecbbf817f764568e9a4766b50b4
brambasiel <95661609+bramtechs@users.noreply.github.com>
2025-01-27 17:57:42 +0100

    Package lua (#5)
    * lua compiles, can't find so yet
    
    * find the so file and add license to source tar

---
+++ lua/build.gradle.kts
@@ -0,0 +1,97 @@
+
+
+
+import com.android.ndkports.CMakeCompatibleVersion
+import com.android.ndkports.CMakePortTask
+
+import java.io.File
+
+val portVersion = "5.4.5"
+
+group = "com.walterschell"
+
+version = "$portVersion${rootProject.extra.get("snapshotSuffix")}"
+
+plugins {
+    id("maven-publish")
+    id("com.android.ndkports.NdkPorts")
+    distribution
+}
+
+ndkPorts {
+    ndkPath.set(File(project.findProperty("ndkPath") as String))
+    source.set(project.file("src.tar"))
+    minSdkVersion.set(21)
+}
+
+tasks.register<CMakePortTask>("buildPort") {
+    cmake {
+        cmd += "-DLUA_USE_POSIX=OFF"
+        cmd += "-DLUA_ENABLE_SHARED=ON"
+        cmd += "-DLUA_BUILD_BINARY=OFF"
+        cmd += "-DLUA_BUILD_COMPILER=OFF"
+        cmd += "-DLUA_SUPPORT_DL=ON"
+    }
+}
+
+tasks.prefabPackage {
+    version.set(CMakeCompatibleVersion.parse(portVersion))
+
+    modules {
+        create("lua_shared")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["prefab"])
+            pom {
+                name.set("lua")
+                description.set("Lua is a powerful, efficient, lightweight, embeddable scripting language.")
+                url.set(
+                    "https://www.lua.org/home.html"
+                )
+                licenses {
+                    license {
+                        name.set("MIT license")
+                        url.set("https://www.lua.org/license.html")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("walterschell")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/walterschell/Lua")
+                    connection.set("scm:git:https://github.com/walterschell/Lua")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("${project.rootDir}/build/docs")
+        }
+    }
+}
+
+distributions {
+    main {
+        contents {
+            from("${project.buildDir}/repository")
+            include("**/*.aar")
+            include("**/*.pom")
+        }
+    }
+}
+
+tasks {
+    distZip {
+        dependsOn("publish")
+        destinationDirectory.set(File(rootProject.buildDir, "distributions"))
+    }
+}
