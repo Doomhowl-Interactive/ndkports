91906a683dc2e89081792bac0e62f3f0527a2536
bramtechs <bramtech@telenet.be>
2025-01-30 18:51:05 +0100

    just change gradle Property to File, prepare to clone the repo

--- buildSrc/src/main/kotlin/com/android/ndkports/SourceExtractTask.kt
+++ buildSrc/src/main/kotlin/com/android/ndkports/SourceExtractTask.kt
@@ -2,25 +2,45 @@ package com.android.ndkports
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import java.io.File
 
 abstract class SourceExtractTask : DefaultTask() {
+
+    @get:Optional
     @get:InputFile
-    abstract val source: RegularFileProperty
+    abstract val source: Property<File>
+
+    @get:Optional
+    @get:Input
+    abstract val gitURL: Property<String>
 
     @get:OutputDirectory
     abstract val outDir: DirectoryProperty
 
     @TaskAction
     fun run() {
+        if (source.isPresent) {
+            extractTar();
+        } else if (gitURL.isPresent && gitURL.get().isNotEmpty()) {
+            cloneGitRepo();
+        } else {
+            throw RuntimeException("No source specified, must be either a tar file or a git URL")
+        }
+    }
+
+    private fun cloneGitRepo() {
+        throw RuntimeException("TODO: clone the git repo")
+    }
+
+    private fun extractTar() {
+        // TODO: Cross-platform support
         val pb = ProcessBuilder(
             listOf(
                 "tar",
                 "xf",
-                source.get().asFile.absolutePath,
+                source.get().absolutePath,
                 "--strip-components=1"
             )
         ).redirectErrorStream(true).directory(outDir.get().asFile)
